c3516bd0d790b5ece1d19a556344d13f658affd1
1112sei <124895156+1112sei@users.noreply.github.com>
2023-09-08 08:29:48 +0900

    Create WORD COUNT TOOL.js

---
+++ WORD COUNT TOOL.js
@@ -0,0 +1,91 @@
+const textarea = document.getElementById('textarea');
+const text = document.getElementById('text');
+const word = document.getElementById('word');
+const end = document.getElementById('end');
+const output = document.getElementById('output');
+let newText = "";
+let newWord = [];
+const lang = window.navigator.language;
+window.onload = () => {
+    if (lang !== 'ja') {
+        document.getElementById('title').textContent = 'WORD COUNT TOOL';
+        textarea.placeholder = 'enter here';
+        document.getElementById('start').textContent = 'This article is';
+        end.textContent = 'character';
+        document.getElementById('textLabel').textContent = 'sentence';
+        document.getElementById('wordLabel').textContent = 'word';
+        document.getElementById('select').style.top = '80px';
+        document.getElementById('site_title').textContent = 'Word count tool';
+    }
+}
+textarea.oninput = function () {
+    if (end.textContent === 'character') {
+        searchLetter();
+    } else if (end.textContent === 'word') {
+        searchWord();
+    } else {
+        searchText();
+    }
+}
+
+text.onchange = function () {
+    if (word.checked) {
+        word.checked = false;
+    }
+    if (text.checked) {
+        end.textContent = 'sentence';
+        searchText();
+    } else {
+        end.textContent = 'character';
+        searchLetter();
+    }
+}
+
+word.onchange = function () {
+    if (text.checked) {
+        text.checked = false;
+    }
+    if (word.checked) {
+
+        end.textContent = 'word';
+
+        searchWord();
+    } else {
+
+        end.textContent = 'character';
+
+        searchLetter();
+    }
+}
+
+function searchLetter() {
+    newText = textarea.value.replace(/\s/g, '');
+    output.textContent = newText.length;
+}
+
+function searchWord() {
+    if (textarea.value === '') {
+        output.textContent = 0;
+    } else {
+        newWord = textarea.value.split(' ');
+        if (textarea.value[textarea.value.length - 1] === ' ') {
+            output.textContent = newWord.length - 1;
+        } else {
+            output.textContent = newWord.length;
+        }
+    }
+}
+
+function searchText() {
+    if (textarea.value === '') {
+        output.textContent = 0;
+    } else {
+        newWord = textarea.value.split(/[.|。|!|?|…|！|？]/);
+        var a = textarea.value[textarea.value.length - 1];
+        if (a === '.' || a === '。' || a === '!' || a === '?' || a === '…' || a === '！' || a === '？') {
+            output.textContent = newWord.length - 1;
+        } else {
+            output.textContent = newWord.length;
+        }
+    }
+}
